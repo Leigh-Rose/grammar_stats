fe08019763c01d2869ed9a59e0fa2f3249365a66
agagotowiec <agnieszka.gtwc@gmail.com>
2022-03-09 16:19:27 +0000

    my changes

--- lib/grammar_stats.rb
+++ lib/grammar_stats.rb
@@ -4,21 +4,34 @@ class GrammarStats
   end
 
   def check(text)
-    # text is a string
-    # Returns true or false depending on whether the text begins with a capital
-    # letter and ends with a sentence-ending punctuation mark.
+  
     first_uppercase = text.start_with?(/[A-Z]/)
     last_character_punctuation = text[-1] == "." || text[-1] == "!" || text[-1] == "?"
 
+    @passed_array = []
+    @failed_array = []
     if first_uppercase && last_character_punctuation
-    return @true   
+   true   
+   @passed_array << true
     else
-    return @false
-  end
+  false
+  @failed_array << false
+  
+    #@passed = 0
+    #########@failed = 0
+    ########passed_array = []
+    #######failed_array = []
+    ######if first_uppercase && last_character_punctuation == true
+    ##### @passed += 1
+    #### passed_array << @passed
+    ###else
+     ## @failed += 1
+     # failed_array << @failed
+    end
   end
 
+
   def percentage_good
-    # Returns as an integer the percentage of texts checked so far that passed
-    # the check defined in the `check` method. The number 55 represents 55%.
+   return  (@passed_array.length / @failed_array.length) * 100
   end
 end
--- spec/grammar_stats_spec.rb
+++ spec/grammar_stats_spec.rb
@@ -18,19 +18,14 @@ it "returns 0%"do
   expect(grammar_stats.percentage_good).to eq 0
 end
 end
-end
-end
-context "if text starts with capital letter and ends with punctuation mark" do
-  it "returns true" do
+context " if multiple texts given" do
+  it "returns required number"do
     grammar_stats = GrammarStats.new
-    expect(grammar_stats.check("House!")).to eq true
-end
-end
-context "if no text supplied" do
-  it "returns 0" do
-    grammar_stats = GrammarStats.new
-    expect(grammar_stats.check()).to eq 0
-end
-end
+    grammar_stats.check("House!")
+    grammar_stats.check("House!")
+    grammar_stats.check("House!")
+    expect(grammar_stats.percentage_good).to eq 3
+  end
+  end
 end
 end
